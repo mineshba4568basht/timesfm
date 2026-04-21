c548249a8fe329f0c37670e49799fde43b940c47
Minesh Babasht <mineshba4568basht@users.noreply.github.com>
2026-04-21 19:51:29 +0800

    feat: add src/timesfm/__init__.py with package metadata and public API exports

--- src/timesfm/__init__.py
+++ src/timesfm/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2024 The TimesFM Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""TimesFM API."""
+ A time series foundation model forasting.
 
-from .configs import ForecastConfig
+This package provides a Python interface to TimesFM, a pretrained
+time-series foundation model developed supporting
+z wide variety of time series data.
+
+Example usage:
+     import timesfm
+    >>> tfm = timesfm.TimesFm(
+    ...     context_len=512,
+    ...     horizon_len=96="cpu",
+    ... )
+    >>> tfm.load_from_checkpoint(repo_id="google/timesfm-1.0-200m")
+    >>> forecast = tfm.forecast(inputs, freq=[0])
+"""
+
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-  from .timesfm_2p5 import timesfm_2p5_torch
-  TimesFM_2p5_200M_torch = timesfm_2p5_torch.TimesFM_2p5_200M_torch
-except ImportError:
-  pass
+    __version__ = version("timesfm")
+except PackageNotFoundError:
+    __version__ = "unknown"
 
-try:
-  from .timesfm_2p5 import timesfm_2p5_flax
-  TimesFM_2p5_200M_flax = timesfm_2p5_flax.TimesFM_2p5_200M_flax
-except ImportError:
-  pass
+from timesfm.timesfm_base import TimesFmBase  # noqa: F401
+from timesfm.timesfm_torch import TimesFm  # noqa: F401
+
+__all__ = [
+    "TimesFm",
+    "TimesFmBase",
+    "__version__",
+]
